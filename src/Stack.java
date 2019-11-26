ea6c95a9f85f3fa03a9eb0dd85dd785042a3455a
NethminiPrathibha <nethminiprathibha123@gmail.com>
2019-11-26 02:28:25 -0800

    new

---
+++ src/Stack.java
@@ -0,0 +1,40 @@
+public class Stack {
+    private int[] stack;
+    private int top;
+    private int size;
+
+    public Stack(){
+        size = 50;
+        stack = new int[50];
+        top = -1;
+    }
+
+    public Stack(int size){
+        this.size = size;
+        stack=new int[size];
+        top= -1;
+
+    }
+
+    public boolean isEmpty(){
+        return (top == -1);
+    }
+     public boolean push(int value){
+        if (!isFull()){
+            top++;
+            stack[top]=value;
+            return true;
+        }else {
+            return false;
+        }
+     }
+
+     public int pop(){
+        return stack[top--];
+     }
+
+     public boolean isFull(){
+        return (top==stack.length-1);
+     }
+
+}
